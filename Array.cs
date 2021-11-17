d558f50690a0a4ab5c677458c54a9b6b9dc3280e
akramwasimgithub <imwasim322@gmail.com>
2021-11-17 13:06:18 +0530

    Game Board Added

--- Array.cs
+++ Array.cs
@@ -8,8 +8,7 @@ namespace TicTacToeFame
    public class TicTacToeGame
     {
 
-        public const char X = 'X';
-        public const char O = 'O';
+        
         char[] Board = new char[10]; //declaration of array name Board of size 10
         public void gameBoard()
         {
@@ -20,25 +19,40 @@ namespace TicTacToeFame
                 Console.WriteLine( Board[i]);
             }
         }
-
+        public const char X = 'X';
+        public const char O = 'O';
         public void Choose_X_or_O_()
         {
-            Console.WriteLine("Enter you Choice from O/X :");
+            Console.WriteLine("Enter  Choice from O/X :");
             char playerSelect = Convert.ToChar(Console.ReadLine());
             if (playerSelect == 'X')
             {
                 Console.WriteLine("Player Choose " + playerSelect);
-                Console.WriteLine("Computer Choose " + O);
+                Console.WriteLine("Computer got: " + O);
             }
             else if (playerSelect == 'O')
             {
                 Console.WriteLine("Player Choose " + playerSelect);
-                Console.WriteLine(" Computer Choose " + X);
+                Console.WriteLine("Computer got " + X);
             }
             else
             {
-                Console.WriteLine("Select invalid input  ");
+                Console.WriteLine("Selected invalid input  ");
             }
+
+
+
+           
+              
+                Console.WriteLine(" "+ Board[1] + " | " + Board[2] + " | " + Board[3] + "   ");
+                Console.WriteLine("---|---|---");
+                Console.WriteLine(" "+ Board[4] + " | " + Board[5] + " | " + Board[6] + "   ");
+                Console.WriteLine("---|---|---");
+                Console.WriteLine(" " + Board[7] + " | " + Board[8] + " | " + Board[9] + "  ");
+                
+           
+
+
         }
     }
 }
