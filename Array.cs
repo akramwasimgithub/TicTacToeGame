dc2249f3112c989b582a88e407f69e05be83bcfb
akramwasimgithub <imwasim322@gmail.com>
2021-11-17 11:22:01 +0530

    One more method is added the choose one

--- Array.cs
+++ Array.cs
@@ -4,7 +4,41 @@ using System.Text;
 
 namespace TicTacToeFame
 {
-    class Array
+
+   public class TicTacToeGame
     {
+
+        public const char X = 'X';
+        public const char O = 'O';
+        char[] Board = new char[10]; //declaration of array name Board of size 10
+        public void gameBoard()
+        {
+
+            for (int i = 1; i < 9; i++)
+            {
+                Board[i] = ' ';
+                Console.WriteLine( Board[i]);
+            }
+        }
+
+        public void Choose_X_or_O_()
+        {
+            Console.WriteLine("Enter you Choice from O/X :");
+            char playerSelect = Convert.ToChar(Console.ReadLine());
+            if (playerSelect == 'X')
+            {
+                Console.WriteLine("Player Choose " + playerSelect);
+                Console.WriteLine("Computer Choose " + O);
+            }
+            else if (playerSelect == 'O')
+            {
+                Console.WriteLine("Player Choose " + playerSelect);
+                Console.WriteLine(" Computer Choose " + X);
+            }
+            else
+            {
+                Console.WriteLine("Select invalid input  ");
+            }
+        }
     }
 }
