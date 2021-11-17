c3ce767524887b406c97d11f979283d65a930641
akramwasimgithub <imwasim322@gmail.com>
2021-11-17 10:33:18 +0530

    Main class added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TicTacToeFame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.gameBoard();
+
+            Console.ReadKey();
+        }
+    }
+}
